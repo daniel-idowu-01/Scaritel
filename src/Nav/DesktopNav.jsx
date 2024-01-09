317e95cf595285f995c1a287e72f74235fc82be4
Ebenezer Noble <ebenezernoblest@gmail.com>
2024-01-09 04:00:44 +0100

    update

--- src/Nav/DesktopNav.jsx
+++ src/Nav/DesktopNav.jsx
@@ -11,7 +11,7 @@ function DesktopNav() {
         <div className={`${memoizedIsFixed ? 'sticky' : 'fixed'} w-full h-[108px] header-padding header-border shadow-lg z-10 bg-white hidden md:flex transition-all`}>
            <div className="flex items-center w-full h-[60px] justify-between c-padding transition-all">
                 <div className="w-[142.7px] h-[48px] gap-[120px]">
-                  <Link to = "/home">
+                  <Link to = "/">
                     <img src={logo} alt="logo" width={logo.width} height={logo.height}/>
                   </Link>
                 </div>       
@@ -25,7 +25,7 @@ function DesktopNav() {
                             > Our Services </Link>
                           </li>
                           <button className="w-[155px] h-[60px] gap-[12px] rounded-[8px] border-[1px] bg-[#1F3AE3] btn-padding">
-                            <Link to = "/"  className="text-[18px] leading-[28px] font-semibold inter text-white"> Let's Talk </Link>
+                            <Link to = "/contact"  className="text-[18px] leading-[28px] font-semibold inter text-white"> Let's Talk </Link>
                           </button>
                     </ul>     
                 </div>
